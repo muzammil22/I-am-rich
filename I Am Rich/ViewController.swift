b0c7c02163c2c45664d0d2a22064dcc20953c4c5
muzammil22 <muzzammilmuneer@gmail.com>
2020-01-30 22:44:42 +0500

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Muzammil Muneer on 30/01/2020.
+//  Copyright © 2020 Muzammil Muneer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
